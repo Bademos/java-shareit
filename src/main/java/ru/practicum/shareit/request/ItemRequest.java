b9c4d3b2a63546ca47e2eaf051fbe1f4580bf134
Bademos <curbademus@yandex.ru>
2023-05-14 15:28:02 +0300

    fiat: initiation 3_rd req

--- src/main/java/ru/practicum/shareit/request/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequest.java
@@ -1,15 +1,46 @@
 package ru.practicum.shareit.request;
 
-import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.model.User;
+import ru.practicum.shareit.util.ConstantsShare;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * TODO Sprint add-item-requests.
  */
 @Data
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "requests", schema = "public")
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class ItemRequest {
-    private int id;
-    private User user;
-    private Item item;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+
+    @NotBlank
+    String description;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false, cascade = CascadeType.ALL)
+    @JoinColumn(name = "requestor_id", nullable = false)
+    User userOfRequest;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = ConstantsShare.datePattern)
+    @Column(name = "create_time")
+    LocalDateTime created;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "request_id")
+    List<Item> items;
 }
